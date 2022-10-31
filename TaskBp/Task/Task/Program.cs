ab157b71815c169a63b15ce7e57f3e01f5e0edb8
“ahadvt” <ahadvt@code.edu.az>
2022-10-31 22:01:59 +0400

    done

---
+++ TaskBp/Task/Task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dəyər daxil edin");
+
+            string Deyer = Console.ReadLine();
+            int Mermi = 30;
+            while (Deyer != "")
+            {
+                if (Deyer == "1")
+                {
+                    Console.WriteLine("Mermi atildi");
+                    Mermi--;
+                }
+                if (Deyer == "2")
+                {
+                    for (int i = Mermi; i > 0; i--)
+                    {
+                        Console.WriteLine("Mermi atildi");
+                    }
+
+                    Mermi = 0;
+                }
+
+                Deyer = Console.ReadLine();
+            }
+
+
+
+            //gencler stringi inte cevirmek ucun
+            //int A = int.Parse("1");
+
+
+        }
+
+    }
+}
